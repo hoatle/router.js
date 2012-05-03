3a07b0c2ea3ce862a82b0f14fbe5c239f11b0cd9
hoatle <hoatlevan@gmail.com>
2012-05-04 01:40:37 +0700

    Simple benchmark of router#map

--- test/speed.js
+++ test/speed.js
@@ -1,16 +1,10 @@
 (function() {
 
-  var patterns = [];
-  for (var i = 0; i < 1000; i++) {
-    patterns.push('/pattern/' + i);
-  }
 
-  JSLitmus.test('router#map', function() {
-    for (var i = 0, len = patterns.length; i < len; i++) {
-      router.map(patterns[i], function() {
+  JSLitmus.test('router#map simple', function() {
+    router.map('/foo/bar/', function() {
 
-      });
-    }
+    });
   });
 
 })();
