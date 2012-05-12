2bd1075ea9657c12011d5145dd98cf03cb51a88a
hoatle <hoatlevan@gmail.com>
2012-05-12 09:40:00 +0700

    Add unit test for router::APIs

--- test/routerTest.js
+++ test/routerTest.js
@@ -16,6 +16,43 @@ $(document).ready(function() {
 
   });
 
+  test('router::APIs', function() {
+
+    expect(19);
+
+    //config
+    ok(router.config, 'router.config must be available');
+
+    //mapping
+    ok(router.map, 'router.map must be available');
+    ok(router.to, 'router.to must be available');
+    ok(router.where, 'router.where must be available');
+
+    //util
+    ok(router.clear, 'router.clear must be available');
+    ok(router.find, 'router.find must be available');
+    ok(router.add, 'router.add must be available');
+    ok(router.remove, 'router.remove must be available');
+
+    //core
+    ok(router.dispatch, 'router.dispatch must be available');
+    ok(router.url, 'router.url must be available');
+
+    //server
+    ok(router.options, 'router.options must be available');
+    ok(router.get, 'router.get must be available');
+    ok(router.head, 'router.head must be available');
+    ok(router.post, 'router.post must be available');
+    ok(router.put, 'router.put must be available');
+    ok(router.del, 'router.del must be available');
+    ok(router.trace, 'router.trace must be available');
+    ok(router.connect, 'router.connect must be available');
+
+    //misc
+    ok(router.noConflict, 'router.noConflict must be available');
+
+  });
+
 
   test('router#config', function() {
 
@@ -58,13 +95,6 @@ $(document).ready(function() {
     router.map('/foo/bar/', function() {
       //empty callback
     });
-
-    //var found2 = router.find('/foo/bar/');
-    //ok(_.isObject(found2), 'found2 must be an object');
-
-
-
-
 
   });
 
